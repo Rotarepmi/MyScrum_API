ecca70e26c208317d13ce94e603cc5d7e5c0263a
Jakub Mandra <j.mandra@mdbootstrap.com>
2019-01-24 17:57:46 +0100

    Add pug engine and simple error view, clean upm index.js

--- src/index.js
+++ src/index.js
@@ -2,23 +2,28 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 
 import express from 'express';
+import mongoose from 'mongoose';
 import { join } from 'path';
 import bodyParser from 'body-parser';
 import register from 'babel-core/register';
 import babelPolyfill from 'babel-polyfill';
-import mongoose from 'mongoose';
 
-import { notFound, catchErrors } from './middlewares/errors';
-
-import auth from './routes/auth';
-import projects from './routes/projects';
-
+// config
 import config from './config/config';
 import dbConfig from './config/database';
 import passport from './config/passport';
 
+// middlewares
+import { notFound, catchErrors } from './middlewares/errors';
+
+// routes
+import auth from './routes/auth';
+import projects from './routes/projects';
+
+// verify user callbacks
 passport();
 
+// connect to mongoDB
 mongoose.connect(dbConfig.mongoUrl);
 mongoose.Promise = global.Promise;
 mongoose.connection.on('error', (err) => {
@@ -26,15 +31,17 @@ mongoose.connection.on('error', (err) => {
     process.exit();
 });
 
+// create app
 const app = express();
 
+// set up view engine
 app.set('view engine', 'pug');
 app.set('views', join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// routes config
+// use routes
 app.use('/api/auth', auth());
 app.use('/api/projects', projects());
 
