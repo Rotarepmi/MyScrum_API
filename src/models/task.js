0544e2a3d8d477952a42b76a083292a1c4303093
Jakub Mandra <j.mandra@mdbootstrap.com>
2019-01-30 19:17:34 +0100

    Add creator to task model

--- src/models/task.js
+++ src/models/task.js
@@ -4,6 +4,11 @@ const TaskSchema = mongoose.Schema({
     name: String,
     description: String,
     status: String,
+    creator: {
+      first_name: String,
+      last_name: String,
+      id: Object
+    },
     project: {
         name: String,
         id: Object
