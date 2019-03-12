35dbd7ee3db8b30baa5b3f5752c31c1731845d24
Jakub Mandra <j.mandra@mdbootstrap.com>
2019-03-12 23:12:28 +0100

    Update Auth login controller
    Provide custom messages

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,14 +1,21 @@
 import User from '../models/user';
 import jwt from 'jsonwebtoken';
 import { validationResult } from 'express-validator/check';
+import passport from 'passport';
 
 export default {
   async login(req, res, next) {
-    // generate token
-    const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, { expiresIn: 1200 });
+    passport.authenticate('local', { session: false }, function (err, user, info) {
+      if (err) return next(err);
 
-    // return token
-    return res.status(200).json({ username: req.user.username, _id: req.user._id, token: token });
+      if (!user) {
+        return res.status(401).json({ message: info.message });
+      }
+
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: 1200 });
+
+      return res.status(200).json({ message: 'Logged in successfully', username: user.username, _id: user._id, token: token });
+    })(req, res, next);
   },
 
   async register(req, res, next) {
