15d3608b87d25845efc787547662ab12dae07583
Jakub Mandra <j.mandra@mdbootstrap.com>
2019-03-12 23:13:00 +0100

    Update auth route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,13 +2,13 @@ import { Router } from 'express';
 import { catchAsync } from '../middlewares/errors';
 import { validateRegister } from '../middlewares/validators';
 import AuthController from '../controllers/authController';
-import passport from 'passport';
 
 export default () => {
-    const api = Router();
+  const api = Router();
 
-    api.post('/login', passport.authenticate('local', { session: false}), catchAsync(AuthController.login));
-    api.post('/register', validateRegister(), catchAsync(AuthController.register));
+  api.post('/login', catchAsync(AuthController.login));
 
-    return api;
+  api.post('/register', validateRegister(), catchAsync(AuthController.register));
+
+  return api;
 }
